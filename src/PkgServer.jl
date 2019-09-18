bc8a4eb2ec943eea7247379f4922804bff366da0
Stefan Karpinski <stefan@karpinski.org>
2019-09-18 18:44:25 -0400

    Files generated by PkgTemplates

---
+++ src/PkgServer.jl
@@ -0,0 +1,5 @@
+module PkgServer
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using PkgServer
+using Test
+
+@testset "PkgServer.jl" begin
+    # Write your own tests here.
+end
