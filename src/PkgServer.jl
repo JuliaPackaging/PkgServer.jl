cb7d3bc4978041d1c9e025a55b14f2fec7696259
Elliot Saba <staticfloat@gmail.com>
2020-01-08 14:20:29 -0800

    Allow parameterization of `host`/`port` when starting a PkgServer

--- src/PkgServer.jl
+++ src/PkgServer.jl
@@ -221,7 +221,7 @@ function serve_file(http::HTTP.Stream, path::String)
     end
 end
 
-function start()
+function start(;host="127.0.0.1", port=8000)
     mkpath("temp")
     mkpath("cache")
     update_registries()
@@ -232,7 +232,7 @@ function start()
             update_registries()
         end
         @info "server listening"
-        HTTP.listen("127.0.0.1", 8000) do http
+        HTTP.listen(host, port) do http
             resource = http.message.target
             if occursin(resource_re, resource)
                 path = fetch(resource)
