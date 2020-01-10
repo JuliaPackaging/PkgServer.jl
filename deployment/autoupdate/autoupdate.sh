cd749ec829a1f73f4d5f360d75fcf932693ebb10
Elliot Saba <staticfloat@gmail.com>
2020-01-09 16:39:50 -0800

    Further iteration of auto-reloading

--- deployment/autoupdate/autoupdate.sh
+++ deployment/autoupdate/autoupdate.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 BRANCH_NAME="$1"
+if [[ -z "${BRANCH_NAME}" ]]; then
+    echo "Usage: autoupdate.sh <branch_name>" >&2
+    exit 1
+fi
 
-cd /code/deployment
-git fetch --all; git checkout "${BRANCH_NAME}" && git reset --hard "origin/${BRANCH_NAME}"
-make up
+rm -rf /tmp/PkgServer.jl
+git clone -b "${BRANCH_NAME}" https://github.com/JuliaPackaging/PkgServer.jl /tmp/PkgServer.jl
+cd /tmp/PkgServer.jl/deployment
+make reload_pkgserver
