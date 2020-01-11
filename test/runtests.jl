4d02c6f330d8356346602cbc45966a759eea1a05
Elliot Saba <staticfloat@gmail.com>
2020-01-10 16:01:24 -0800

    Properly polymorphize ECONNREFUSED in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -42,9 +42,9 @@ while true
         HTTP.get("$(server_url)/registries"; retry = false, readtimeout=1).status
     catch e
         # If it just can't connect, deal with it silently, otherwise rethrow
-        if isa(e, HTTP.IOExtras.IOError) && e.e.code == -61
-            # Return fake 500 status code, this doesn't really matter, just so long as it's not 200
-            500
+        if isa(e, HTTP.IOExtras.IOError) && e.e.code == -Base.Libc.ECONNREFUSED
+            # Return fake status code, this doesn't really matter, just so long as it's not 200
+            -Base.Libc.ECONNREFUSED
         else
             rethrow(e)
         end
