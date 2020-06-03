11116e0abba6beeb1cf0d313e22f808f8c6171c2
Elliot Saba <staticfloat@gmail.com>
2020-06-03 19:19:31 +0000

    During tests, Use actual storage server instead of upstream pkg server
    This avoids issues when we attempt to "bootstrap" a testing instance of
    the Pkg server; if there's an issue during transmission of the base
    registries, for instance, this can fail while attempting to test the
    very fix needed on the live pkg servers right now.

--- bin/run_server.jl
+++ bin/run_server.jl
@@ -3,7 +3,7 @@
 # Accept optional environment-based arguments
 host = get(ENV, "PKGSERVER_HOST", "127.0.0.1")
 port = parse(Int, get(ENV, "PKGSERVER_PORT", "8000"))
-storage_servers = strip.(split(get(ENV, "PKGSERVER_STORAGESERVERS", "https://pkg.julialang.org"), ","))
+storage_servers = strip.(split(get(ENV, "PKGSERVER_STORAGESERVERS", "https://us-east.storage.julialang.org"), ","))
 
 using PkgServer
 empty!(PkgServer.STORAGE_SERVERS)
