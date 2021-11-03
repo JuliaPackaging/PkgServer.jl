7699ba1e420e9c4ca41ada2f983aca8d9c0f383d
Elliot Saba <staticfloat@gmail.com>
2021-11-03 03:47:29 +0000

    Update dependencies and codebase to assume Julia v1.6+

--- test/tests.jl
+++ test/tests.jl
@@ -140,7 +140,7 @@ end
             Pkg.add(Pkg.PackageSpec(;name="DotNET", version=v"0.1.0"))
             Pkg.add(Pkg.PackageSpec(;name="FIGlet", version=v"0.2.1"))
         end
-        prepare_for_deletion(temp_dir)
+        Base.Filesystem.prepare_for_deletion(temp_dir)
     end
 
     # Sleep until there's nothing left in the `temp` directory.
